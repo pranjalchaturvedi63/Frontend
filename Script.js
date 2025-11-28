6f39172ea9cecfa2483a6f54ded3b1d5b45a341e
pranjalchaturvedi63 <pranjalchaturvedi63@gmail.com>
2025-11-28 20:56:28 +0530

    popUps

---
+++ Script.js
@@ -0,0 +1,22 @@
+// var a=5;
+// let b=3;
+// const c=10;
+
+// document.write(Math.pow(a,b));
+// document.write("<br>"+Math.sqrt(49));
+
+
+let a=100;
+let b="100";
+
+document.write(a);
+document.write("<br>"+b);
+document.write("<br>Type of a : "+typeof a);
+document.write("<br>Type of b : "+typeof b);
+
+document.write("<br>",a==b); //value
+document.write("<br>",a===b); //value + type
+
+
+
+
---
+++ popups/script.js
@@ -0,0 +1,16 @@
+// alert("Welcome to javaScript"); //return nothing or undefined
+
+// let ans =confirm("Do you want to proceed?");
+// document.write(ans); //return true or false 
+
+
+// let a =prompt("Enter your name"); //return string
+// document.writeln(a);
+
+let a =parseInt(prompt("Enter a first number "));
+let b =parseInt(prompt("Enter a second number "));
+
+let sum =a+b;
+document.writeln(sum);
+
+//task : Try to print a table using prompt
